991630e60109811cb3230a00bfa92e105130b68e
CurwinB <87906110+CurwinB@users.noreply.github.com>
2026-08-11 16:25:03 -0400

    Finalize CineXtream Anime SUB/DUB URL builder

--- cinextream-config.js
+++ cinextream-config.js
@@ -1,12 +1,8 @@
 // Single source of truth for CineXtream playback.
 // Anime language is part of the path: /api/embed/anime/sub|dub/[ANILIST_ID]/[EPISODE]
 window.BOUYONTV_CINEXTREAM_BASE = 'https://cinextream.cc';
-
 window.BouyonTVCineXtream = {
-  movie: (tmdbId) => `${window.BOUYONTV_CINEXTREAM_BASE}/api/embed/movie/${encodeURIComponent(tmdbId)}`,
-  tv: (tmdbId, season = 1, episode = 1) => `${window.BOUYONTV_CINEXTREAM_BASE}/api/embed/tv/${encodeURIComponent(tmdbId)}/${encodeURIComponent(season)}/${encodeURIComponent(episode)}`,
-  anime: (anilistId, episode = 1, language = 'sub') => {
-    const lang = String(language).toLowerCase() === 'dub' ? 'dub' : 'sub';
-    return `${window.BOUYONTV_CINEXTREAM_BASE}/api/embed/anime/${lang}/${encodeURIComponent(anilistId)}/${encodeURIComponent(episode)}`;
-  }
+  movie: id => `${window.BOUYONTV_CINEXTREAM_BASE}/api/embed/movie/${encodeURIComponent(id)}`,
+  tv: (id,s=1,e=1) => `${window.BOUYONTV_CINEXTREAM_BASE}/api/embed/tv/${encodeURIComponent(id)}/${encodeURIComponent(s)}/${encodeURIComponent(e)}`,
+  anime: (id,e=1,lang='sub') => `${window.BOUYONTV_CINEXTREAM_BASE}/api/embed/anime/${String(lang).toLowerCase()==='dub'?'dub':'sub'}/${encodeURIComponent(id)}/${encodeURIComponent(e)}`
 };
